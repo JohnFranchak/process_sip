54999ac5b4a87afafe2c5b1d67c08dba76616174
John Franchak <franchak@gmail.com>
2026-01-30 11:05:10 -0800

    Update find_sync_points.m

--- find_sync_points.m
+++ find_sync_points.m
@@ -16,11 +16,11 @@ opts.EmptyLineRule = "read";
 % Specify variable properties
 opts = setvaropts(opts, "Time", "InputFormat", "yyyy-MM-dd HH:mm:ss.SSS");
 
-ppt = "01";
+ppt = "12";
 
 %%
 % Import the LEFT ANKLE
-leftankle = readtable(strcat(ppt,"/",ppt,"_left_ankle.csv"), opts);
+leftankle = readtable(strcat(ppt,"_LA.csv"), opts);
 
 close all
 clf
@@ -34,23 +34,8 @@ ax3 = nexttile;
 plot(leftankle.Time, leftankle.Z)
 linkaxes([ax1 ax2 ax3],'x')
 %%
-% Import the LEFT HIP
-lefthip = readtable(strcat(ppt,"/",ppt,"_left_hip.csv"), opts);
-
-close all
-clf
-tiledlayout(3,1)
-% First plot
-ax1 = nexttile;
-plot(lefthip.Time, lefthip.X)
-ax2 = nexttile;
-plot(lefthip.Time, lefthip.Y)
-ax3 = nexttile;
-plot(lefthip.Time, lefthip.Z)
-linkaxes([ax1 ax2 ax3],'x')
-%%
 % Import the RIGHT ANKLE
-rightankle = readtable(strcat(ppt,"/",ppt,"_right_ankle.csv"), opts);
+rightankle = readtable(strcat(ppt,"_RA.csv"), opts);
 
 close all
 clf
@@ -64,8 +49,56 @@ ax3 = nexttile;
 plot(rightankle.Time, rightankle.Z)
 linkaxes([ax1 ax2 ax3],'x')
 %%
+% Import the LEFT HIP
+lefthip = readtable(strcat(ppt,"_LH.csv"), opts);
+
+close all
+clf
+tiledlayout(3,1)
+% First plot
+ax1 = nexttile;
+plot(lefthip.Time, lefthip.X)
+ax2 = nexttile;
+plot(lefthip.Time, lefthip.Y)
+ax3 = nexttile;
+plot(lefthip.Time, lefthip.Z)
+linkaxes([ax1 ax2 ax3],'x')
+
+%%
 % Import the RIGHT HIP
-righthip = readtable(strcat(ppt,"/",ppt,"_right_hip.csv"), opts);
+righthip = readtable(strcat(ppt,"_RA.csv"), opts);
+
+close all
+clf
+tiledlayout(3,1)
+% First plot
+ax1 = nexttile;
+plot(righthip.Time, righthip.X)
+ax2 = nexttile;
+plot(righthip.Time, righthip.Y)
+ax3 = nexttile;
+plot(righthip.Time, righthip.Z)
+linkaxes([ax1 ax2 ax3],'x')
+
+%%
+% Import the CAREGIVER WRIST
+lefthip = readtable(strcat(ppt,"_CW.csv"), opts);
+
+close all
+clf
+tiledlayout(3,1)
+% First plot
+ax1 = nexttile;
+plot(lefthip.Time, lefthip.X)
+ax2 = nexttile;
+plot(lefthip.Time, lefthip.Y)
+ax3 = nexttile;
+plot(lefthip.Time, lefthip.Z)
+linkaxes([ax1 ax2 ax3],'x')
+
+%%
+% Import the CAREGIVER HIP
+righthip = readtable(strcat(ppt,"_CH.csv"), opts);
 
 close all
 clf
